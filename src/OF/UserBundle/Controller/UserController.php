fe824281b77fb501bc1d415c4ddafa47cb38c021
overflozz <valentin.chelle@hotmail.fr>
2017-04-24 08:21:32 +0200

    Login dans le menu du layout

---
+++ src/OF/UserBundle/Controller/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OF\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\UserBundle\Controller\SecurityController as SecurityController;
+
+use OF\UserBundle\Entity\User;
+
+/**
+ * Description of UserController
+ *
+ */
+class UserController extends SecurityController {
+	// Permet d'utiliser l'include du form login.content.html.twig sur toutes les pages
+    public function LoginBisAction()
+    {
+    	//CF \vendor\friendsofsymfony\user-bundle\Controller\SecurityController pour comprendre le crsf et les différents arguments ici.
+        $csrfToken = $this->has('security.csrf.token_manager')
+            ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
+            : null;
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Security:login_content.html.twig', array(
+            'last_username' => null,
+            'error'         => null,
+            'csrf_token'    => $csrfToken
+        ));
+    }
+
+}
